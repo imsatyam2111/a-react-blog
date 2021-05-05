49ff23d56c0f0d2d1d8535fd7436064e0f36cec5
Satyam <ss9125rpvv@gmail.com>
2021-05-06 01:21:30 +0530

    updation

---
+++ src/BlogList.js
@@ -0,0 +1,23 @@
+const BlogList = ( { blogs, title, handleDelete } ) => {
+    // Alternative to passing props in the parenthesis and destructuring it later
+    // We can Destructure props object within the parenthesis like
+    // const blogs = props.blogs is similar to ( { blogs } )
+    
+    // const blogs = props.blogs;
+    // const blogs = props.title;
+
+    return (
+        <div className="blog-list">
+            <h2>{ title }</h2>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By: { blog.author }</p>
+                    <button onClick={ () => handleDelete(blog.id) }>Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'First React app', body: 'loreum ipsum ...', author: 'Satyam', id: 0 },
-        { title: 'Welcome guide to React', body: 'loreum ipsum ...', author: 'Satyam', id: 1 },
+        { title: 'Welcome guide to React', body: 'loreum ipsum ...', author: 'Saurabh', id: 1 },
         { title: 'Features of React', body: 'loreum ipsum ...', author: 'Satyam', id: 2 },
-        { title: 'Implementation of React', body: 'loreum ipsum ...', author: 'Satyam', id: 3 }
+        { title: 'Implementation of React', body: 'loreum ipsum ...', author: 'Saurabh', id: 3 }
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+    }    
+
+    useEffect(() => {
+        console.log('use Effec ran');
+        console.log(blogs);
+    }, []);
 
     return (
         <div className="home">
-            { blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written By: { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete } />
+            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Satyam' ) } title="Satyam's Blogs" /> 
         </div>
     );
 }
